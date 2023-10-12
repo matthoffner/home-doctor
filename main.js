522913aab3a104505646c2b4c2ef221230e7c684
Matt Hoffner <matthoffner@gmail.com>
2023-10-11 18:17:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
-let worker = new Worker('worker.js');
+import { initializeChat } from './chat.js';
 
-worker.addEventListener('message', function(event) {
-    if (event.data.type === 'update' && event.data.target === 'output') {
-        document.getElementById('output').innerText = event.data.data;
+const worker = new Worker('worker.js');
+
+let textBoxNode = document.getElementById('textbox');
+let loaderNode = document.getElementById('loader');
+
+worker.onmessage = (event) => {
+    const data = event.data;
+
+    if (data.type === 'result' && data.task === 'image-to-text') {
+        const extractedText = data.data;
+        textBoxNode.innerHTML += `<div class="message received">Extracted from image: ${extractedText}</div>`;
+        loaderNode.style.display = 'none'; // hide the loader
+        
+        // Now, use the extracted text to interact with the GPT chatbot
+        // Assuming your chatbot function accepts a message parameter
+        initializeChat(extractedText);
     }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const processImageBtn = document.getElementById("process-image");
+    const imageUpload = document.getElementById("image-upload");
+
+    processImageBtn.addEventListener('click', async () => {
+        if (imageUpload.files.length > 0) {
+            loaderNode.style.display = 'block'; // show loader
+
+            const image = imageUpload.files[0];
+            const reader = new FileReader();
+
+            reader.readAsDataURL(image);
+            reader.onload = () => {
+                const base64Image = reader.result.split(',')[1];
+                worker.postMessage({
+                    task: 'image-to-text',
+                    image: base64Image
+                });
+            };
+        }
+    });
 });
-
-function processImage() {
-    const fileInput = document.getElementById('imageUpload');
-    const file = fileInput.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            const image = event.target.result;
-
-            // Send image data to the worker for processing
-            worker.postMessage({
-                task: 'image-to-text',
-                image: image,
-                elementIdToUpdate: 'output',
-                generation: {} // any additional options
-            });
-        };
-
-        reader.readAsDataURL(file);
-    } else {
-        alert('Please choose an image first.');
-    }
-}
