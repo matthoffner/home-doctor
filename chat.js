b209aa0151e49481ad0dc824eeb5ba32cfde3204
Matt Hoffner <matthoffner@gmail.com>
2023-10-12 01:57:01 +0000

    simplify

--- chat.js
+++ chat.js
@@ -1,22 +1,5 @@
 // chat.js
 
-let sendBotMessage; // Declare sendBotMessage here so that it can be accessed outside of initializeChat
-
-const worker = new Worker('./worker.js', { type: 'module' });
-
-worker.addEventListener('message', (event) => {
-    const { data } = event;
-    
-    if (data && data.type === 'result' && data.task === 'image-to-text') {
-        const extractedText = data.data; // The text extracted from the image
-        
-        // Use the extractedText as an initial prompt for the chatbot, if sendBotMessage is defined
-        if (sendBotMessage) {
-            sendBotMessage(extractedText);
-        }
-    }
-});
-
 export const initializeChat = (initialPrompt) => {
     const APIURL = 'https://api.openai.com/v1/chat/completions';
     const loaderNode = document.getElementById('loader');
@@ -88,29 +71,6 @@ export const initializeChat = (initialPrompt) => {
             loaderNode.style.display = 'none';  // Hide loader after processing
         }
     };
-
-    const processImageBtn = document.getElementById("process-image");
-    const imageUpload = document.getElementById("image-upload");
-    
-    processImageBtn.addEventListener('click', async () => {
-        if (imageUpload.files.length > 0) {
-            loaderNode.style.display = 'block'; // show the loader
-
-            const image = imageUpload.files[0];
-            const reader = new FileReader();
-
-            reader.readAsDataURL(image);
-            reader.onload = () => {
-                const base64Image = reader.result.split(',')[1];
-                // Now, we send this base64 encoded image to your worker for processing
-                // After processing, the worker will send back the extracted text, which can then be used as the initial prompt for the chatbot.
-                worker.postMessage({
-                    task: 'image-to-text',
-                    image: base64Image
-                });
-            };
-        }
-    });
 
     let sendButtonNode = document.getElementById('send-button');
     let userInputNode = document.querySelector('.user-input');
