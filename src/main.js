8c737b5b530e4233c1f5f654e005fa407ae978f5
Matt Hoffner <matthoffner@gmail.com>
2023-10-12 15:50:35 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -37,28 +37,46 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const worker = new Worker(new URL('./worker.js', import.meta.url), {
         type: 'module',
-    });      
+    });
 
     worker.onmessage = (event) => {
         const data = event.data;
 
-        if (data.type === 'result' && data.task === 'image-to-text') {
-            const extractedText = data.data;
-            const generatedText = extractedText[0].generated_text
-            textBoxNode.innerHTML += `<div class="message received">Extracted from image: ${generatedText}</div>`;
-            loaderNode.style.display = 'none'; // hide the loader
-            if (generatedText) {
-                initializeChat(`I've uploaded an image, I know you can't see but here is the image caption result: ${generatedText}, reply back with a helpful response from the caption I provided`);
-            }
+        loaderNode.style.display = 'none'; // hide the loader
+
+        switch(data.task) {
+            case 'image-to-text':
+                const extractedText = data.data;
+                const generatedText = extractedText[0].generated_text;
+                textBoxNode.innerHTML += `<div class="message received">Extracted from image: ${generatedText}</div>`;
+                if (generatedText) {
+                    initializeChat(`I've uploaded an image, I know you can't see but here is the image caption result: ${generatedText}, reply back with a helpful response from the caption I provided`);
+                }
+                break;
+
+            case 'image-classification':
+                // Handle image classification results
+                const topClassification = data.data[0].label;
+                textBoxNode.innerHTML += `<div class="message received">Top classification: ${topClassification}</div>`;
+                break;
+
+            case 'object-detection':
+                // Handle object detection results
+                const detectedObjects = data.data.map(obj => obj.label).join(', ');
+                textBoxNode.innerHTML += `<div class="message received">Detected objects: ${detectedObjects}</div>`;
+                break;
         }
     };
 
     const processImageBtn = document.getElementById("process-image");
     const imageUpload = document.getElementById("image-upload");
+    const taskSelect = document.getElementById("task-select"); // Assuming you have a dropdown with this ID to select the task
 
     processImageBtn.addEventListener('click', async () => {
         if (imageUpload.files.length > 0) {
             loaderNode.style.display = 'block'; // show loader
+
+            const selectedTask = taskSelect.value; // Read selected task from dropdown
     
             const image = imageUpload.files[0];
             const reader = new FileReader();
@@ -67,22 +85,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 const imageElement = new Image();
                 imageElement.onload = () => {
                     const base64Image = getImageDataFromImage(imageElement).split(',')[1];
-    
-                    // Append the image to chat before sending it to the worker
                     appendImageToChat(base64Image);
-    
+
+                    // Post the message based on the selected task
                     worker.postMessage({
-                        task: 'image-to-text',
+                        task: selectedTask,
                         image: `data:image/jpeg;base64,${base64Image}`
                     });
                 };
                 imageElement.src = reader.result;
             };
-    
             reader.readAsDataURL(image);
         }
     });    
-    
 
     initializeChat();
 });
